4d99d07f1a0a4909d34b5db96efd56b440cd5c61
Claire Casey <clcasey805@gmail.com>
2021-01-07 15:06:07 -0800

    first 3 routes for invoices complete

--- routes/companies.js
+++ routes/companies.js
@@ -3,7 +3,7 @@
 
 const express = require("express");
 const db = require("../db");
-const { BadRequestError } = require("../expressError");
+const { BadRequestError, NotFoundError } = require("../expressError");
 const middleware = require("../middleware");
 const router = express.Router();
 /**  GET /companies
@@ -25,7 +25,7 @@ Return obj of company: {company: {code, name, description}}
 If the company given cannot be found, returns a 404 status response. 
 */
 
-router.get("/:code", middleware.codeVerify, async function (req, res, next) {
+router.get("/:code", async function (req, res, next) {
   const { code } = req.params;
   const results = await db.query(
     `SELECT code, name, description
@@ -33,6 +33,8 @@ router.get("/:code", middleware.codeVerify, async function (req, res, next) {
              WHERE code = $1`,
     [code]);
   const company = results.rows[0];
+
+  if (!company) throw new NotFoundError(`Not found: ${code}`);
   return res.json({ company })
 });
 
@@ -44,15 +46,22 @@ Needs to be given JSON like: {code, name, description}
 Returns obj of new company: {company: {code, name, description}}
 */
 
-router.post('/', middleware.nameVerify, async function (req, res, next) {
+router.post('/', async function (req, res, next) {
   const { code, name, description } = req.body;
-
-  const result = await db.query(
-    `INSERT INTO companies (code, name, description)
-           VALUES ($1, $2, $3)
-           RETURNING code, name, description`,
-    [code, name, description],
-  );
+  // QUESTION: Is it better to include this try/catch, or to add a middleware with a query? 
+  let result;
+  try {
+    result = await db.query(
+      `INSERT INTO companies (code, name, description)
+             VALUES ($1, $2, $3)
+             RETURNING code, name, description`,
+      [code, name, description],
+    );
+  } catch (err) {
+    throw new BadRequestError(
+      `Name (${name}) or code (${code}) already exits`)
+  }
+  console.log('result', result);
   const company = result.rows[0];
   return res.status(201).json({ company });
 })
@@ -67,24 +76,25 @@ Needs to be given JSON like: {name, description}
 Returns update company object: {company: {code, name, description}}
 */
 
-router.put('/:code', middleware.codeVerify, 
-                     middleware.nameVerify, 
-                     async function (req, res, next) {
-  const { name, description } = req.body;
-  const { code } = req.params;
+router.put('/:code',
+  middleware.nameVerify,
+  async function (req, res, next) {
+    const { name, description } = req.body;
+    const { code } = req.params;
 
-  const result = await db.query(
-    `UPDATE companies
+    const result = await db.query(
+      `UPDATE companies
            SET name=$1,
                description=$2
            WHERE code = $3
            RETURNING code, name, description`,
-    [name, description, code],
-  );
-  const company = result.rows[0];
-  
-  return res.json({ company });
-});
+      [name, description, code],
+    );
+    const company = result.rows[0];
+    if (!company) throw new NotFoundError(`Not found: ${code}`)
+
+    return res.json({ company });
+  });
 
 /** DELETE /companies/[code]
 Deletes company.
@@ -94,11 +104,18 @@ returns 404 if company cannot be found.
 Returns {status: "deleted"} 
 */
 
-router.delete('/:code', middleware.codeVerify, async function (req, res, next) {
-  await db.query(
-    "DELETE FROM companies WHERE code = $1",
+router.delete('/:code', async function (req, res, next) {
+  const { code } = req.params;
+
+  const result = await db.query(
+    `DELETE FROM companies WHERE code = $1
+      RETURNING code, name, description`,
     [req.params.code],
   );
+
+  const company = result.rows[0];
+  if (!company) throw new NotFoundError(`Not found: ${code}`);
+
   return res.json({ status: "deleted" });
 })
 
--- routes/invoices.js
+++ routes/invoices.js
@@ -1,2 +1,92 @@
 "use strict";
+/* Routes for invoices */
 
+const express = require("express");
+const db = require("../db");
+const { BadRequestError, NotFoundError } = require("../expressError");
+// const middleware = require("../middleware");
+const router = express.Router();
+
+
+/**  GET /invoices
+Return info on invoices: like {invoices: [{id, comp_code}, ...]}
+*/
+
+router.get("/", async function (req, res, next) {
+  const results = await db.query(
+    `SELECT id, comp_code
+           FROM invoices`);
+  const invoices = results.rows;
+
+  return res.json({ invoices });
+});
+
+
+/**  GET /invoices/[id]
+Returns obj on given invoice, or 404 if invoice cannot be found.
+Returns {invoice: 
+  {id, amt, paid, add_date, paid_date, company: {code, name, description}}
+*/
+
+router.get("/:id", async function (req, res, next) {
+  const { id } = req.params;
+  const iResults = await db.query(
+    `SELECT id, amt, paid, add_date, paid_date
+             FROM invoices
+             WHERE id = $1`,
+    [id]);
+  const invoice = results.rows[0];
+
+  if (!invoice) throw new NotFoundError(`Not found: ${code}`);
+
+  const codeResults = await db.query(
+    `SELECT comp_code
+             FROM invoices
+             WHERE id = $1`,
+    [id]);
+
+  const compCode = codeResults.rows[0].comp_code
+
+  const cResults = await db.query(
+    `SELECT code, name, description
+             FROM companies
+             WHERE code = $1`,
+    [compCode]);
+
+  invoice.company = cResults.rows[0];
+
+  return res.json({ invoice })
+});
+
+
+/** POST /companies
+Adds an invoice.
+Needs to be passed in JSON body of: {comp_code, amt}
+Returns: {invoice: {id, comp_code, amt, paid, add_date, paid_date}}
+*/
+
+router.post('/', async function (req, res, next) {
+  const { comp_code, amt } = req.body;
+  // NOTE: currently, we are not allowing invoices of 0.
+  if (!Number(amt)) throw new BadRequestError(`Invalid amount: ${amt}`);
+
+  // QUESTION: Is it better to include this try/catch, or to add a middleware with a query? 
+  let result;
+  try {
+    result = await db.query(
+      `INSERT INTO invoices (comp_code, amt)
+             VALUES ($1, $2)
+             RETURNING id, comp_code, amt, paid, add_date, paid_date`,
+      [comp_code, amt],
+    );
+  } catch (err) {
+    throw new BadRequestError(
+      `Company code (${comp_code}) does not exist`)
+  }
+
+  const invoice = result.rows[0];
+  return res.status(201).json({ invoice });
+});
+
+
+module.exports = router;
